b488ceb3c996cbb2d0ce71135dafd706e44ab039
Nurdaulet Turar <nurdaulet.turar.cz@gmail.com>
2024-09-29 19:14:19 +0200

    Add complex test case & fix test's error message

--- tests/test_lexer.c
+++ tests/test_lexer.c
@@ -42,14 +42,15 @@ void test_lexeme(const char *test_name, const char **input, const int count, ...
         char *expected = va_arg(args, char*);
 
         // Compare the expected with an actual value
-        const bool match = strcmp(lexeme.value, expected) == 0;
-        destroy_lexeme(lexeme);
+        const bool match = strcmp(expected, lexeme.value) == 0;
 
         if (!match) {
             fprintf(stderr, "[Failure] %-20s: Incorrect lexeme. Got: [%s] Expected: [%s]\n", test_name, lexeme.value, expected);
+            destroy_lexeme(lexeme);
             destroy_lexeme_parser(&parser);
             return;
         }
+        destroy_lexeme(lexeme);
     }
 
     fprintf(stderr, "\033[32m[Success] %-20s\033[0m\n", test_name);
@@ -62,6 +63,9 @@ int main() {
     test_lexeme("Single input", &input, 1, "i32");
     test_lexeme("Line", &input, 5, "i32", "a", "=", "15", ";");
     test_lexeme("Multi Line", &input, 11, "i32", "a", "=", "15", ";", "int", "main", "(", ")", "{", "}");
+
+    const char *functionInput = "const ifj=@import(\"ifj24.zig\");pub fn main()void{ifj.write(\"Zadejte cislo pro vypocet faktorialu\n\");const a=ifj.readi32();if(a)|val|{if(val<0){ifj.write(\"Faktorial \");ifj.write(val);ifj.write(\" nelze spocitat\n\");}else{var d:f64=ifj.i2f(val);var vysl:f64=1.0;while(d>0){vysl=vysl*d;d=d-1.0;}ifj.write(\"Vysledek: \");ifj.write(vysl);ifj.write(\" = \");const vysl_i32=ifj.f2i(vysl);ifj.write(vysl_i32);ifj.write(\"\n\");}}else{ifj.write(\"Faktorial pro null nelze spocitat\n\");}}";
+    test_lexeme("Project pdf example with factorials #1", &functionInput, 8, "const","ifj","=","@","import","(","ifj24.zig",")");
 
     return 0;
 }
