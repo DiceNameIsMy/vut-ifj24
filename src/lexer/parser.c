cd0af6a47824fc3058cea3f8867fdcd947255546
Fundgod <fundgod.savin@gmail.com>
2024-10-10 02:04:13 +0200

    Almost done. Need some testing

--- src/lexer/parser.c
+++ src/lexer/parser.c
@@ -27,8 +27,9 @@ typedef enum {
 
 // Array of keywords
 const char* keywords[] = {
-    "const", "else", "fn", "if", "i32", "f64", "null", "pub", 
-    "return", "u8", "var", "void", "while"
+    "const", "var", "if", "else", "while", "fn", "pub", 
+    "null", "return", "void", 
+    "i32", "?i32", "f64", "?f64", "u8", "[]u8", "?[]u8"
 };
 
 #define NUM_KEYWORDS (sizeof(keywords) / sizeof(keywords[0]))   // Amount of key words
@@ -92,7 +93,7 @@ bool isSeparator(char c) {
     return false;
 }
 
-// function to check if this is an identifier ##
+// function to check if this is an identifier
 bool isIdentifier(const char* str) {
     regex_t regex;
     // Regular expression that checks if string is a valid identifier
@@ -108,144 +109,40 @@ bool isIdentifier(const char* str) {
     return result == 0;
 }
 
-// Function to check if it is a number ##
-int isNumber(const char* str) {
-    regex_t regex;
-    regcomp(&regex, "^[0-9]+(\\.[0-9]+)?$", 0);  // Whole or fractional number ##
-    int result = regexec(&regex, str, 0, NULL, 0);
-    regfree(&regex);
-    return result == 0;
-}
-
-// Function for processing lexemes ##
-void processToken(const char* token) {
-    token_type_t token_type;    // TODO: Maybe create attribute None?
-    if (isKeyword(token)) {
-        token_type = processKeyword(token);
-        printf("Keyword: %s\n", token); // Process keyword #
-    } else if (isIdentifier(token)) {
-        printf("Identifier: %s\n", token); // Process id #
-    } else if (isDefineType(token)) {
-        printf("Type: %s\n", token); // Process type i32, f64 etc. #
-    } else if (isNumber(token)) {
-        printf("Number: %s\n", token); // Process num #
-    } else if (isSpecialSymbol(token)) {
-        printf("Special symbol: %s\n", token); // Process special symbol #
-    } else {
-        printf("Unknown token: %s\n", token); // Process Error #
-        exit(1);
-    }
+int identifyNumberType(const char* str) {
+    regex_t i32_regex, f64_regex;
     
-}
+    // i32 literal regex
+    const char* i32_pattern = "^[0-9]+$";
+    // f64 literal regex with number before '.'
+    const char* f64_pattern = "^([0-9]+\\.[0-9]*([eE][+-]?[0-9]+)?)|(\\.[0-9]+([eE][+-]?[0-9]+)?)|([0-9]+[eE][+-]?[0-9]+)$";
+    
+    // Compile regex
+    regcomp(&i32_regex, i32_pattern, REG_EXTENDED);
+    regcomp(&f64_regex, f64_pattern, REG_EXTENDED);
 
-// The main function of the lexer ##
-void lexer(const char* source_code) {
-    LexerState state = STATE_NORMAL;  // Initial state
-    char buffer[BUFFER_SIZE];  // Token Buffer
-    int buffer_index = 0;  // Buffer index
-
-    int i = 0;  // Index for source code symbols
-    while (source_code[i] != '\0') {
-        char c = source_code[i];
-        // State handling
-        switch (state) {
-            case STATE_NORMAL:
-                if (isSeparator(c)) { // a+b
-                    if (buffer_index > 0) {
-                        buffer[buffer_index] = '\0';  // put end of the buffer for future cmp
-                        processToken(buffer);
-                        buffer_index = 0;
-                        // Check if it isn't whitespace and increase buffer otherwise
-                        // Maybe add state AFTER_SPECIAL SIMBOL or process it right away
-                    }
-                } else if (c == '"') {  // String starts
-                    if (buffer_index != 0){
-                        buffer[buffer_index] = '\0';  // Process all from buffer
-                        processToken(buffer);
-                        buffer_index = 0;
-
-                        state = STATE_STRING;   // Start String status 
-                    }
-                } else if (c == '/' && source_code[i + 1] == '/') {  // Comment starts
-                    buffer[buffer_index] = '\0';  // Process all from buffer
-                    processToken(buffer);
-                    buffer_index = 0;
-
-                    i++;    // Increasing i by 1, so next reading won't start from '\' 
-                    state = STATE_COMMENT;
-                    // Not sure if it's needed
-                /*} else if (ispunct(c)) {  // TODO: MAYBE ADD CHECK 1 symb?
-                    // Handling special character ##
-                    if (buffer_index > 0) {
-                        buffer[buffer_index] = '\0';
-                        processToken(buffer);  // Token processing ##
-                        buffer_index = 0;
-                    }
-                    printf("Special symbol: %c\n", c);*/
-                } else {
-                    buffer[buffer_index] = c;  // Add a character to the buffer ##
-                    buffer_index++;
-                }
-                break;
-
-            case STATE_STRING:
-                if (c == '\\' && source_code[i + 1] == '"'){    // if '"' is part of the string    TODO: Can it raise an ERROR at the end? MAYBE ERROR
-                    buffer[buffer_index] = c;
-                    buffer_index++;
-                    buffer[buffer_index] = '"';
-                    buffer_index++;
-                    i++;
-                }else if (c == '"') { // end of the string
-                    buffer[buffer_index] = '\0';
-                    printf("String: \"%s\"\n", buffer);  // TODO: Gotta be another parser for str only
-                    buffer_index = 0;
-                    state = STATE_NORMAL;
-                } else if (c == "\n"){
-                    state = STATE_NEXT_LINE_STRING; // Starts next line
-                } else {
-                    buffer[buffer_index++] = c;  // String processing ##
-                }
-                break;
-            case STATE_NEXT_LINE_STRING:
-                if (isspace(c) == false){
-                    if (c == '\\' && source_code[i + 1] == '\\'){   //TODO: Can it raise an ERROR at the end?
-                        i++;
-                        state = STATE_STRING;
-                    }
-                    else{
-                        ; // TODO: ERROR OCCURE bc string isn't closed
-                    }
-                }
-                break; 
-
-            case STATE_COMMENT:
-                if (c == '\n') {
-                    state = STATE_NORMAL;   // Return to normal state after comment ##
-                                            // Indexes must be zeros, so no changes needed
-                }
-                break;
-        }
-        i++;
+    // Check if string matches i32 pattern
+    if (regexec(&i32_regex, str, 0, NULL, 0) == 0) {
+        regfree(&i32_regex);
+        regfree(&f64_regex);
+        return 1;  // It's an i32 literal
     }
 
-    // Processing the last token in the buffer ##
-    if (buffer_index > 0 && state == STATE_NORMAL) {
-        buffer[buffer_index] = '\0';
-        processToken(buffer);
+    // Check if string matches f64 pattern
+    if (regexec(&f64_regex, str, 0, NULL, 0) == 0) {
+        regfree(&i32_regex);
+        regfree(&f64_regex);
+        return 2;  // It's an f64 literal
     }
-    else {
-        ; // TODO: ERROR OCCURE
-    }
+
+    // Free regex memory
+    regfree(&i32_regex);
+    regfree(&f64_regex);
+    
+    return 0;  // Neither i32 nor f64 literal
 }
 
-int main() {
-    const char* code = "fn main() { const x = 42; // This is a comment\n print(\"Hello, World!\"); }";
-    lexer(code);
-    return 0;
-}
-
-
-int isSpecialSymbol(char c) {
+bool isSpecialSymbol(char c) {
     return strchr("+-*/=()[]{}", c) != NULL;
 }
 
@@ -316,12 +213,135 @@ token_t processSpecialSymbol(char c) {
     return token;
 }
 
-// Is it a num TODO: more characteristics
-int isNumber(char* str) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (!isdigit(str[i])) {
-            return 0;  // Err
-        }
+
+// Function for processing lexemes
+void processToken(const char* buf_str) {
+    token_type_t token_type;
+    token_attribute attribute;
+    attribute.str = NULL;
+
+    if (isKeyword(buf_str)) {
+        token_type = processKeyword(buf_str);
+        printf("Keyword: %s\n", buf_str); // Process keyword and types i32, f64 etc.
+    } 
+    else if (isIdentifier(buf_str)) {
+        token_type = TOKEN_ID;
+        printf("Identifier: %s\n", buf_str); // Process id
     }
-    return 1;
+    else if (identifyNumberType(buf_str)) {     // != 0
+        if (identifyNumberType(buf_str) == 1){
+            token_type = TOKEN_I32_LITERAL;
+            attribute->integer = atoi(buf_str);
+        }
+        if (identifyNumberType(buf_str) == 2){
+            token_type = TOKEN_F64_LITERAL;
+            attribute->real = strtod(str, NULL);
+        }
+        printf("Number: %s\n", buf_str); // Process num
+    } 
+    else if (isSpecialSymbol(buf_str) && buf_str[1] == '\0') {
+        token_type = processSpecialSymbol(buf_str[0]);
+        printf("Special symbol: %s\n", buf_str); // Process special symbol
+    } else {
+        printf("Unknown token: %s\n", buf_str); // Unxpected input, Error
+        exit(1);
+    }
+    token_t token = createToken(buf_str, attribute); 
+}
+
+// The main function of the lexer ##
+void lexer(const char* source_code) {
+    LexerState state = STATE_NORMAL;  // Initial state
+    char buffer[BUFFER_SIZE];  // Token Buffer
+    int buffer_index = 0;  // Buffer index
+
+    int i = 0;  // Index for source code symbols
+    while (source_code[i] != '\0') {
+        char c = source_code[i];
+        // State handling
+        switch (state) {
+            case STATE_NORMAL:
+                if (isSeparator(c)) { // a+b
+                    if (buffer_index > 0) {
+                        buffer[buffer_index] = '\0';  // put end of the buffer for future cmp
+                        processToken(buffer);
+                        buffer_index = 0;
+                        // Check if it isn't whitespace and increase buffer otherwise
+                        // Maybe add state AFTER_SPECIAL SIMBOL or process it right away
+                    }
+                } else if (c == '"') {  // String starts
+                    if (buffer_index != 0){
+                        buffer[buffer_index] = '\0';  // Process all from buffer
+                        processToken(buffer);
+                        buffer_index = 0;
+
+                        state = STATE_STRING;   // Start String status 
+                    }
+                } else if (c == '/' && source_code[i + 1] == '/') {  // Comment starts
+                    buffer[buffer_index] = '\0';  // Process all from buffer
+                    processToken(buffer);
+                    buffer_index = 0;
+
+                    i++;    // Increasing i by 1, so next reading won't start from '\' 
+                    state = STATE_COMMENT;
+                } else {
+                    buffer[buffer_index] = c;  // Add a character to the buffer ##
+                    buffer_index++;
+                }
+                break;
+
+            case STATE_STRING:
+                if (c == '\\' && source_code[i + 1] == '"'){    // if '"' is part of the string    TODO: Can it raise an ERROR at the end? MAYBE ERROR
+                    buffer[buffer_index] = c;
+                    buffer_index++;
+                    buffer[buffer_index] = '"';
+                    buffer_index++;
+                    i++;
+                }else if (c == '"') { // end of the string
+                    buffer[buffer_index] = '\0';
+                    printf("String: \"%s\"\n", buffer);  // TODO: Gotta be another parser for str only
+                    buffer_index = 0;
+                    state = STATE_NORMAL;
+                } else if (c == "\n"){
+                    state = STATE_NEXT_LINE_STRING; // Starts next line
+                } else {
+                    buffer[buffer_index++] = c;  // String processing ##
+                }
+                break;
+            case STATE_NEXT_LINE_STRING:
+                if (isspace(c) == false){
+                    if (c == '\\' && source_code[i + 1] == '\\'){   //TODO: Can it raise an ERROR at the end?
+                        i++;
+                        state = STATE_STRING;
+                    }
+                    else{
+                        ; // TODO: ERROR OCCURE bc string isn't closed
+                    }
+                }
+                break; 
+
+            case STATE_COMMENT:
+                if (c == '\n') {
+                    state = STATE_NORMAL;   // Return to normal state after comment ##
+                                            // Indexes must be zeros, so no changes needed
+                }
+                break;
+        }
+        i++;
+    }
+
+    // Processing the last token in the buffer ##
+    if (buffer_index > 0 && state == STATE_NORMAL) {
+        buffer[buffer_index] = '\0';
+        processToken(buffer);
+    }
+    else {
+        ; // TODO: ERROR OCCURE
+    }
+}
+
+int main() {
+    const char* code = "fn main() { const x = 42; // This is a comment\n print(\"Hello, World!\"); }";
+    lexer(code);
+    return 0;
 }
