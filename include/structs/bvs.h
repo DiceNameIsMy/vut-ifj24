9978eb317c1d33b0320da712924825b19096b032
GladiatorEntered <148907803+GladiatorEntered@users.noreply.github.com>
2024-10-26 15:05:25 +0000

    Implement a vv-BVS
    * void-ification of BVS_Init and BVS_BranchInsert
    
    * including parents in struct BVSBranch, void-ification continued
    
    * Red Uncle and LL-case if Black Uncle resolved
    
    * Red Uncle and LL-case if Black Uncle resolved
    
    * Root cases in InsertResolve covered
    
    * Declarations modified
    
    * Declarations modified
    
    * Declarations modified, definition of IsRoot() added
    
    * Deletion mechanism implemented, without colours resolving function
    
    * Syntax errors...
    
    * Syntax errors part 2...
    
    * Changes for the pr
    
    * Changes for the pr part 2
    
    * Delete function rewritten
    
    * OH FUCK REALLY
    
    * DeleteResolve, doubleblack scenario remains
    
    * Partially solved doubleblack, got confused with rotations. Fuck.
    
    * Modified rotations. Anyone plz check them. Please.
    
    * )
    
    * Resolving finished, may be veeery buggy cuz rotations're still screwed up
    
    * Errare humanum est
    
    * Fixed rotations. TODO: un-recur OR write some tests
    
    * Finished undoubleblacking. TODO: write some tests
    
    * It works a bit
    
    * Un-recurred insert + some logs. Insertion seems to work properly now.
    
    * test on random insertion btw
    
    * And the comments, if you insist
    
    * Segfaults solved, some items are lost though
    
    * Insertion. Works. Don't touch.
    
    * Deletion: Red sibling fix
    
    * Segfault fix, still can double-free smth
    
    * Solved the situation with rotations (viz 3.2 on Geeks)
    
    * Solved some of the double free issues, also there are amazing comments in the code so look please
    
    * No more segfaults (unless number of inputs reaches +-1045000). Either it is because it leaks as fuck, or it is just a coincidence.
    
    * No segfaults. No leaks. No headache.
    
    * Now tests also don't leak. Logs commented. Need to polish a little bit, then gonna merge.
    
    * Typos corrected
    
    * Optimizations. We love optimizations!

--- include/structs/bvs.h
+++ include/structs/bvs.h
@@ -12,6 +12,7 @@ typedef struct BVSBranch {
     char color;
     struct BVSBranch *left;
     struct BVSBranch *right;
+    struct BVSBranch *parent; //wer gonna need this for colour resolving routines a lot
 } BVSBranch;
 
 // Vyskove Vyvazeni Binarni Vyhledavaci Strom
@@ -21,8 +22,9 @@ typedef struct {
 
 int BVS_Init(BVS *bvs);
 void BVS_Free(const BVS *bvs);
-
+void BVS_Delete(BVS *BVS, const long key);
 void BVS_Insert(BVS *bvs, long data);
 bool BVS_Search(BVS *bvs, long data);
+//bool BVS_IsBalanced(BVS *bvs);
 
 #endif //BVS_H
--- tests/test_bvs.c
+++ tests/test_bvs.c
@@ -4,23 +4,24 @@
 
 #include <stdio.h>
 #include <structs/bvs.h>
+#include <time.h>
+#include <stdlib.h>
 
 #include "test_utils.h"
 
 BVS bvs;
 
 TEST(insert_1)
-    BVS_Free(&bvs);
     BVS_Init(&bvs);
 
     BVS_Insert(&bvs, 1);
     if (!BVS_Search(&bvs, 1)) {
         FAIL("Insertion and then search had failed");
     }
+    BVS_Free(&bvs);
 ENDTEST
 
 TEST(insert_2)
-    BVS_Free(&bvs);
     BVS_Init(&bvs);
 
     BVS_Insert(&bvs, 1);
@@ -31,10 +32,103 @@ TEST(insert_2)
     if (!BVS_Search(&bvs, 2)) {
         FAIL("Insertion and then search had failed");
     }
+    BVS_Free(&bvs);
+ENDTEST
+
+TEST(insert_many)
+    BVS_Init(&bvs);
+
+    BVS_Insert(&bvs, 10);
+    BVS_Insert(&bvs, 20);
+    BVS_Insert(&bvs, 3);
+    BVS_Insert(&bvs, 15);
+    BVS_Insert(&bvs, 0);
+    BVS_Insert(&bvs, 14);
+    BVS_Insert(&bvs, 12);
+    if(!BVS_Search(&bvs, 10)) {
+	fprintf (stderr, "Failed to find (10)\n");
+        FAIL("Not found");
+    }
+    if(!BVS_Search(&bvs, 20)) {
+	fprintf (stderr, "Failed to find (20)\n");
+        FAIL("Not found");
+    }
+    if(!BVS_Search(&bvs, 3)) {
+	fprintf (stderr, "Failed to find (3)\n");
+        FAIL("Not found");
+    }
+    if(!BVS_Search(&bvs, 15)) {
+	fprintf (stderr, "Failed to find (15)\n");
+        FAIL("Not found");
+    }
+    if(!BVS_Search(&bvs, 0)) {
+	fprintf (stderr, "Failed to find (0)\n");
+        FAIL("Not found");
+    }
+    if(!BVS_Search(&bvs, 14)) {
+	fprintf (stderr, "Failed to find (14)\n");
+        FAIL("Not found");
+    }
+    if(!BVS_Search(&bvs, 12)) {
+	fprintf (stderr, "Failed to find (12)\n");
+        FAIL("Not found");
+    }
+    //if(!BVS_IsBallanced(&bvs)) {
+    //    FAIL("Ballance is corrupted!\n"); IRRELEVANT DUE TO RB TREE PROPERTIES
+    //    exit(-1);
+    //}
+
+    BVS_Free(&bvs);
+ENDTEST
+
+TEST(insert_many_and_test)
+    srand(time(NULL));
+    BVS_Init(&bvs);
+    int max = 1000000;
+    long bvs_arr[1000000] = {0};
+    int q = -1;
+    for (int i = 0; i < max; i++) {
+        bvs_arr[i] = rand()%50000000;
+        //fprintf(stderr, "NEW INPUT (%ld)\n", bvs_arr[i]);
+        //BVS_Insert(&bvs, i);
+        BVS_Insert(&bvs, bvs_arr[i]);
+        
+        //if (!BVS_IsBallanced(&bvs)) {
+        //    FAIL("Ballance is corrupted!\n"); IRRELEVANT DUE TO RB-TREE PROPERTIES
+        //    exit(-1);
+        //}
+	q*=q;
+    }
+    for (int i = 0; i < max; i++) {
+        if(!BVS_Search(&bvs, bvs_arr[i])) {
+            FAIL("Not found");
+	    fprintf (stderr, "Failed to find (%ld)\n", bvs_arr[i]);
+           // exit(-1);
+        }
+    }
+    for (int i = 0; i < max; i++) {
+	//fprintf(stderr, "DELETING (%ld)\n", bvs_arr[i]);
+	if (!BVS_Search(&bvs, bvs_arr[i])) {
+	    bool found = false;
+	    for (int j = 0; j < i; j++) {
+		if (bvs_arr[j] == bvs_arr[i]) {
+			found = true;
+			break;
+		}
+	    }
+	    if (!found) {
+	        FAIL("Deleted too early");
+	    }
+	}
+        BVS_Delete(&bvs, bvs_arr[i]);
+        if (BVS_Search(&bvs, bvs_arr[i])) {
+            FAIL("Failed to delete");
+        }
+    }
+    BVS_Free(&bvs);
 ENDTEST
 
 TEST(search_not_existing_element)
-    BVS_Free(&bvs);
     BVS_Init(&bvs);
 
     if (BVS_Search(&bvs, 1)) {
@@ -46,6 +140,11 @@ TEST(search_not_existing_element)
     if (BVS_Search(&bvs, 3)) {
         FAIL("Search on non-empty BVS found a non-existent item");
     }
+    BVS_Free(&bvs);
+ENDTEST
+
+TEST(delete_a_node)
+    
 ENDTEST
 
 int main() {
@@ -54,4 +153,5 @@ int main() {
     RUN_TESTS();
 
     SUMMARIZE()
+
 }
