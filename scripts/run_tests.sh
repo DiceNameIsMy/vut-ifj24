50f89fd1fa27c3cbfe4bdc88bf644bde5bc2ffc5
Nurdaulet Turar <nurdaulet.turar.cz@gmail.com>
2024-11-16 14:27:34 +0100

    Tmp: Add debug information to CI runs

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -7,10 +7,11 @@ exit_status=0
 while IFS= read -r file; do
     # Check if the file is executable
     if [[ -x "$file" ]]; then
+        echo "Running test $file." >&2
         # Execute the file. If failed, Capture the exit status of the command
         if ! "$file"; then
             # Set exit_status to 1 if any command fails
-            echo "Execution failed for file: $file"
+            echo "Execution failed for file: $file" >&2
             exit_status=1
         fi
     else
@@ -21,7 +22,7 @@ while IFS= read -r file; do
 done
 
 echo
-echo "All tests runners have succeeded"
+echo "All tests runners have succeeded" >&2
 
 # Exit with the final status (0 if all succeeded, 1 if any failed)
 exit $exit_status
