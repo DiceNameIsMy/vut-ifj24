cc467f47be4220e2491213a475e995059f8be11d
GladiatorEntered <xkrasoo00@stud.fit.vutbr.cz>
2024-11-19 16:39:17 +0100

    New scope initialization

--- include/structs/symtable.h
+++ include/structs/symtable.h
@@ -4,8 +4,9 @@
 typedef enum {U8, I32, F64, NONETYPE} type_t;
 typedef enum {LOCAL, GLOBAL, UNDEFINED} scope_t;
 
-typedef struct {
+typedef struct SymTable_t {
   BVS *tree;
+  SymTable_t *parent;
 } SymTable;
 
 // TODO: Add counter of amount of usages (or a boolean whether a symbol was used)
@@ -17,6 +18,7 @@ typedef struct {
   bool init;
   scope_t scope;
 } Symbol;
+
 
 void SymTable_SetType(SymTable *table, char *name, type_t type);
 void SymTable_SetDecl(SymTable *table, char *name, bool isDeclared);
@@ -30,5 +32,5 @@ scope_t SymTable_GetScope(SymTable *table, char *name);
 
 void SymTable_Init(SymTable *table);
 void SymTable_AddSymbol(SymTable *table, Symbol *symbol);
-//SymTable *SymTable_NewScope(SymTable *table); I needa think how this will even work. By now (17.10.2024) I'm a bit confused.
+SymTable *SymTable_NewScope(SymTable *table); //I needa think how this will even work. By now (17.10.2024) I'm a bit confused.
 void Symtable_Dispose(SymTable *table); //"delete root" algorithm
