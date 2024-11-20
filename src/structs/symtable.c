bb70c15ee8acba511558136924f9437a975c2b4b
GladiatorEntered <xkrasoo00@stud.fit.vutbr.cz>
2024-11-20 10:25:58 +0100

    Spaghetti stack added, pls review symtable.c

--- src/structs/symtable.c
+++ src/structs/symtable.c
@@ -1,66 +1,94 @@
+//
+//created by GladiatorEntered on 18/11/24
+//
+
+
 #include "structs/symtable.h"
 #include "bvs.c"
+#include "structs/stack.h"
 //TODO: implement functions from the header
 
+#define MAX_SCOPES 65536 
+//2^16
+
+void Scope_Init(Scope *scope);
+Scope *Scope_ChildScope(Scope *scope);
+void Scope_Dispose(Scope *scope);
+
+void SymTable_Init(SymTable *table) {
+  initializeStack(table->stack, MAX_SCOPES);
+  table->current = NULL;
+  return;
+}
+
+void SymTable_NewScope(SymTable *table) {
+  table->current = Scope_ChildScope(table->current);
+  StackPush(table->stack, table->current, sizeof(Scope));
+  return;
+}
+
 void SymTable_SetType(SymTable *table, char *name, type_t type) {
-  ((Symbol *)(BVS_Search(table->tree, name)))->type = type;
+  ((Symbol *)(BVS_Search(table->current->tree, name)))->type = type;//TODO: implement "current" mechanism
   return;
 }
 
 void SymTable_SetDecl(SymTable *table, char *name, bool isDeclared) {
-  ((Symbol *)(BVS_Search(table->tree, name)))->decl = isDeclared;
+  ((Symbol *)(BVS_Search(table->current->tree, name)))->decl = isDeclared;
   return;
 }
 
 void SymTable_SetInit(SymTable *table, char *name, bool isInit) {
-  ((Symbol *)(BVS_Search(table->tree, name)))->init = isInit;
-  return;
-}
-
-void SymTable_SetScope(SymTable *table, char *name, scope_t scope) {
-  ((Symbol *)(BVS_Search(table->tree, name)))->scope = scope;
+  ((Symbol *)(BVS_Search(table->current->tree, name)))->init = isInit;
   return;
 }
 
 type_t SymTable_GetType(SymTable *table, char *name) {
-  return ((Symbol *)(BVS_Search(table->tree, name)))->type;
+  return ((Symbol *)(BVS_Search(table->current->tree, name)))->type;
 }
 
 bool SymTable_GetDecl(SymTable *table, char *name) {
-  return ((Symbol *)(BVS_Search(table->tree, name)))->decl;
+  return ((Symbol *)(BVS_Search(table->current->tree, name)))->decl;
 }
 
 bool SymTable_GetInit(SymTable *table, char *name) {
-  return ((Symbol *)(BVS_Search(table->tree, name)))->init;
-}
-
-scope_t SymTable_GetScope(SymTable *table, char *name) {
-  return ((Symbol *)(BVS_Search(table->tree, name)))->scope;
-}
-
-void SymTable_Init(SymTable *table) {
-  BVS_Init(table->tree);
-  return;
+  return ((Symbol *)(BVS_Search(table->current->tree, name)))->init;
 }
 
 void SymTable_AddSymbol(SymTable *table, Symbol *symbol) {
   symbol->name = strdup(symbol->name);
-  BVS_Insert(table->tree, symbol->name, (void *)symbol, sizeof(Symbol));
+  BVS_Insert(table->current->tree, symbol->name, (void *)symbol, sizeof(Symbol));
   return;
 }
 
 void SymTable_Dispose(SymTable *table) {
-  while (table->tree->root != NULL)
-  {
-    free(((Symbol *)table->tree->root->data)->name);
-    BVS_Delete(table->tree, table->tree->root->key);
+  while (!isStackEmpty(table->stack)) {
+    Scope *scope = (Scope *)(popStack(table->stack));
+    Scope_Dispose(scope);
   }
+  //should we free a symtable? 
   return;
 }
 
-SymTable *SymTable_ChildScope(SymTable *table) {
-  SymTable *newScope = (SymTable *)malloc(sizeof(SymTable));
-  SymTable_Init(newScope);
-  newScope->parent = table;
+/////////////////////////////////////////////////////////////////////////////
+
+void Scope_Init(Scope *scope) {
+  BVS_Init(scope->tree);
+  return;
+}
+
+void Scope_Dispose(Scope *scope) {
+  while (scope->tree->root != NULL)
+  {
+    free(((Symbol *)scope->tree->root->data)->name);
+    BVS_Delete(scope->tree, scope->tree->root->key);
+  }
+  free(scope);
+  return;
+}
+
+Scope *Scope_ChildScope(Scope *scope) {
+  Scope *newScope = (Scope *)malloc(sizeof(Scope));
+  Scope_Init(newScope);
+  newScope->parent = scope;
   return newScope;   
 }
