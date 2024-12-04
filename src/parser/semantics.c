599cac742d99e9c76599372e932356bc7c619b54
GladiatorEntered <xkrasoo00@stud.fit.vutbr.cz>
2024-12-04 20:17:36 +0100

    Arithmetic and assignment now work properly

--- src/parser/semantics.c
+++ src/parser/semantics.c
@@ -13,40 +13,51 @@ type_t Sem_MathConv(ASTNode *left, ASTNode *right, ASTNode *higher_order) {
       
       if((typeL == typeR) && (typeL == I32 || typeL == F64 || typeL == I32_LITERAL || typeL == F64_LITERAL)) { //equal numeric types 
         higher_order->valType = typeL;
-        if(typeL == I32_LITERAL || typeL == F64_LITERAL) {
-          PerformArithm(left, right, higher_order);
+        fprintf(stderr, "Types are equal\n");
+        PerformArithm(left, right, higher_order);
+        return higher_order->valType;
+      }
+
+      if((typeL == I32_LITERAL || typeL == I32) && (typeR == I32_LITERAL || typeR == I32)) {
+        higher_order->valType = I32;
+        fprintf(stderr, "Types are I32-like\n");
+        PerformArithm(left, right, higher_order);
+        if(typeL == I32_LITERAL && typeR == I32_LITERAL) {
+          higher_order->valType = I32_LITERAL;
         }
         return higher_order->valType;
       }
-      
-      if((typeL == I32_LITERAL || typeL == F64_LITERAL || typeL == F64) && (typeR == I32_LITERAL || typeR == F64_LITERAL || typeR == F64)) {
+
+      if((typeL == F64_LITERAL || typeL == F64) && (typeR == F64_LITERAL || typeR == F64)) {
+        fprintf(stderr, "Types are F64-like\n");
         higher_order->valType = F64;
-        if(typeL == I32_LITERAL) {
-          left->valType = F64_LITERAL;
-          left->value->real = (double)(left->value->integer);
-        }
-        if(typeR == I32_LITERAL) {
-          right->valType = F64_LITERAL;
-          right->value->real = (double)(right->value->integer);
-        }
-        if(typeR != F64 && typeL != F64) {
+        PerformArithm(left, right, higher_order);
+        if(typeL == F64_LITERAL && typeR == F64_LITERAL) {
           higher_order->valType = F64_LITERAL;
-          PerformArithm(higher_order->left, right, higher_order);
         }
-        return higher_order->valType; //I32Lit X I32Lit was already handled
+        return higher_order->valType;
+      }
+
+      if(typeL == I32_LITERAL || typeR == I32_LITERAL) {
+        ASTNode *IntLit = (typeL == I32_LITERAL) ? left : right;
+        ASTNode *other  = (left == IntLit) ? right : left;
+        if(other->valType == F64 || other->valType == F64_LITERAL) {
+          higher_order->valType = other->valType;
+          IntLit->valType = F64_LITERAL;
+          IntLit->value->real = (double)(IntLit->value->integer);
+          PerformArithm(left, right, higher_order);
+          return higher_order->valType;
+        }
       }
       
       if (typeL == I32 || typeR == I32) {
         ASTNode *IntVal = (typeL == I32) ? left : right;
         ASTNode *other = (IntVal == left) ? right : left;
-        if(other->valType == I32_LITERAL) {
-          higher_order->valType = I32;
-          return I32;
-        }
         if(other->valType == F64_LITERAL && isRound(other->value->real)) {
           higher_order->valType = I32;
           other->valType = I32_LITERAL;
           other->value->integer = (int)(other->value->real);
+          PerformArithm(left, right, higher_order);
           return I32;
         }
       }      
@@ -160,17 +171,20 @@ type_t Sem_AssignConv(ASTNode *left, ASTNode *right, ASTNode *higher_order) {
     case I32_NULLABLE:
       if(typeR == I32 || typeR == I32_NULLABLE || typeR == NULL_LITERAL) {
         higher_order->valType = typeL; //both for I32 and I32_NULLABLE
+        higher_order->value = valCpy(right->value);
         return typeL;
       }
       if(typeR == I32_LITERAL) {
-        higher_order->valType = I32_LITERAL;
-        return I32_LITERAL;
+        higher_order->valType = typeL;
+        higher_order->value = valCpy(right->value);
+        return typeL;
       }
       if(typeR == F64_LITERAL && isRound(right->value->real)) {
         right->valType = I32_LITERAL;
         right->value->integer = (int)(right->value->real);
-        higher_order->valType = I32_LITERAL;
-        return I32_LITERAL;
+        higher_order->valType = typeL;
+        higher_order->value = valCpy(right->value);
+        return typeL;
       }
       return NONE;
     case F64_LITERAL:
@@ -182,17 +196,20 @@ type_t Sem_AssignConv(ASTNode *left, ASTNode *right, ASTNode *higher_order) {
     case F64_NULLABLE:
       if(typeR == F64 || typeR == F64_NULLABLE || typeR == NULL_LITERAL) {
         higher_order->valType = typeL;
+        higher_order->value = valCpy(right->value);
         return typeL;
       }
       if(typeR == F64_LITERAL) {
-        higher_order->valType = F64_LITERAL;
-        return F64_LITERAL;
+        higher_order->valType = typeL;
+        higher_order->value = valCpy(right->value);
+        return typeL;
       }
       if(typeR == I32_LITERAL) {
         right->valType = F64_LITERAL;
         right->value->real = (double)(right->value->integer);
-        higher_order->valType = F64_LITERAL;
-        return F64_LITERAL;
+        higher_order->valType = typeL;
+        higher_order->value = valCpy(right->value);
+        return typeL;
       }
       return NONE;
     case U8_ARRAY:
@@ -213,12 +230,12 @@ type_t Sem_AssignConv(ASTNode *left, ASTNode *right, ASTNode *higher_order) {
         return NONE;
       }
       higher_order->valType = typeR;
+      higher_order->value = valCpy(right->value);
       return typeR;
     default:
       fprintf(stderr, "London bridge is falling down with nodetype %d\n", (int)higher_order->nodeType);
       exit(99);
     } 
-  higher_order->value = valCpy(right->value);
 }
 
 bool isRound(double literal) {
@@ -229,10 +246,11 @@ bool isRound(double literal) {
 
 void PerformArithm(ASTNode *left, ASTNode *right, ASTNode *higher_order) {
   if(right->value == NULL || left->value == NULL) {
+    fprintf(stderr, "NULL...");
     return;
   }
   higher_order->value = (ASTValue *)malloc(sizeof(ASTValue));
-  if(left->valType == I32_LITERAL && right->valType == I32_LITERAL) {
+  if((left->valType == I32 || left->valType == I32_LITERAL) && (right->valType == I32 || right->valType == I32_LITERAL)) {
     int A = left->value->integer;
     int B = right->value->integer;
     switch(higher_order->nodeType) {
@@ -264,15 +282,19 @@ void PerformArithm(ASTNode *left, ASTNode *right, ASTNode *higher_order) {
   double B = (right->valType == I32_LITERAL) ? (double)(right->value->integer) : right->value->real;
   switch (higher_order->nodeType) {
     case AddOperation:
+      fprintf(stderr, "%lf + %lf = %lf\n", A, B, A + B);
       higher_order->value->real = A + B;
       return;
     case SubOperation:
+      fprintf(stderr, "%lf - %lf = %lf\n", A, B, A - B);
       higher_order->value->real = A - B;
       return;
     case MulOperation:
+      fprintf(stderr, "%lf * %lf = %lf\n", A, B, A * B);
       higher_order->value->real = A * B;
       return;
     case DivOperation:
+      fprintf(stderr, "%lf / %lf = %lf\n", A, B, A / B);
       if (B == 0.0) {
         exit(10);
       }
